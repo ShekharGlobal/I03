4dbcc76c8a3b4dba711bdf8fe89f2aeddb8faccd
ShekharGlobal <traininglabit@gmail.com>
2025-02-15 19:52:27 +0530

    Add files via upload

---
+++ Web Application/JSP/jsbean/User.java
@@ -0,0 +1,22 @@
+package com.example.jspaction;
+public class User {
+    private String userName;
+    private String password;
+
+    // Getters and setters
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

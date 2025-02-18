7fdf25246038bcfc33bfa854a390f38d61aeafac
ShekharGlobal <traininglabit@gmail.com>
2025-02-18 10:06:32 +0530

    Add files via upload

---
+++ Hibernate/bidirectionalmtom/Project.java
@@ -0,0 +1,60 @@
+package com.example.bi.manytomany;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "projects", cascade = CascadeType.ALL)
+    private Set<Employee> employees = new HashSet<Employee>();
+
+    // Constructors, Getters, and Setters
+
+    public Project() {}
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        this.employees.add(employee);
+        employee.getProjects().add(this);
+    }
+
+    public void removeEmployee(Employee employee) {
+        this.employees.remove(employee);
+        employee.getProjects().remove(this);
+    }
+}
